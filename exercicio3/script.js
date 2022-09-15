0448a70586527828c7520c23f6d6225577bd3137
isabellepimentel <isabellechavespimentel@gmail.com>
2022-09-15 19:28:26 -0300

    exercicio a018

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -11,3 +11,11 @@ const produtos = [
     { nome: "Sabão em Pó", categoria: "Limpeza", preco: 10.80 }
  ]
  
+ const retornaProdutos = produtos.filter((produto) => {
+    return produto.categoria === "Limpeza"
+  })
+  console.log(retornaProdutos)
+
+
+  
+    
